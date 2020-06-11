3a15e51f8a8457e7ecc44b3c5abc015712813842
1computer1 <22125769+1Computer1@users.noreply.github.com>
2020-06-11 08:11:55 -0400

    args: add Args for convenient argument retrieving

---
+++ src/args.ts
@@ -0,0 +1,158 @@
+import { ParserOutput } from './parser';
+import { Token } from './tokens';
+
+/**
+ * A wrapper around the parser output for retrieving command arguments.
+ */
+export default class Args {
+    /**
+     * The parser output.
+     */
+    public readonly parserOutput: ParserOutput;
+
+    /**
+     * The indices of the ordered tokens already retrieved.
+     */
+    public readonly usedIndices: Set<number> = new Set();
+
+    /**
+     * The current position in the ordered tokens.
+     */
+    public position = 0;
+
+    /**
+     * @param parserOutput - The parser output.
+     */
+    public constructor(parserOutput: ParserOutput) {
+        this.parserOutput = parserOutput;
+    }
+
+    /**
+     * Whether all ordered tokens have been used.
+     */
+    public get finished(): boolean {
+        return this.usedIndices.size === this.parserOutput.ordered.length;
+    }
+
+    /**
+     * The amount of ordered tokens.
+     */
+    public get length(): number {
+        return this.parserOutput.ordered.length;
+    }
+
+    /**
+     * The amount of remaining ordered tokens.
+     */
+    public get remaining(): number {
+        return this.parserOutput.ordered.length - this.usedIndices.size;
+    }
+
+    /**
+     * Retrieves the value of the next unused ordered token.
+     * That token will now be consider used.
+     * @returns The value if there are tokens left.
+     */
+    public single(): string | null {
+        if (this.finished) {
+            return null;
+        }
+
+        while (this.usedIndices.has(this.position)) {
+            this.position++;
+        }
+
+        this.usedIndices.add(this.position);
+        return this.parserOutput.ordered[this.position++].value;
+    }
+
+    /**
+     * Retrieves many unused tokens.
+     * @param limit - The limit on the amount of tokens to retrieve; defaults to infinite.
+     * @param from - Where to start looking for tokens; defaults to current position.
+     * @returns The tokens.
+     */
+    public many(limit = Infinity, from = this.position): Token[] {
+        const ts = [];
+        for (let i = from; i < this.length && ts.length < limit; i++) {
+            if (this.usedIndices.has(i)) {
+                continue;
+            }
+
+            this.usedIndices.add(i);
+            ts.push(this.parserOutput.ordered[i]);
+        }
+
+        return ts;
+    }
+
+    /**
+     * Checks if a flag was given.
+     * @param key - The name of the flag.
+     * @returns Whether the flag was given.
+     */
+    public flag(key: string): boolean {
+        return this.parserOutput.flags.has(key);
+    }
+
+    /**
+     * Gets the value of an option.
+     * @param key - The name of the option.
+     * @returns The value of the option if it was given.
+     */
+    public option(key: string): string | null {
+        return this.parserOutput.options.has(key)
+            ? this.parserOutput.options.get(key)!
+            : null;
+    }
+
+    /**
+     * Finds and retrieves the first unused token that could be transformed.
+     * That token will now be consider used.
+     * @param f - Gives a pair of whether the transformation worked, and the resulting value.
+     * @param from - Where to start looking for tokens; defaults to current position.
+     * @returns The resulting value if it was found.
+     */
+    public findMap<T>(f: (x: string) => [true, T] | [false, null], from = this.position): T | null {
+        for (let i = from; i < this.length; i++) {
+            if (this.usedIndices.has(i)) {
+                continue;
+            }
+
+            const x = this.parserOutput.ordered[i];
+            const [ok, y] = f(x.value);
+            if (ok) {
+                this.usedIndices.add(i);
+                return y;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Filters and retrieves all unused tokens that could be transformed.
+     * Those tokens will now be consider used.
+     * @param f - Gives a pair of whether the transformation worked, and the resulting value.
+     * @param limit - The limit on the amount of tokens to retrieve; defaults to infinite.
+     * @param from - Where to start looking for tokens; defaults to current position.
+     * @returns The resulting values.
+     */
+    public filterMap<T>(f: (x: string) => [true, T] | [false, null], limit = Infinity, from = this.position): T[] {
+        const ys = [];
+        for (let i = from; i < this.length && ys.length < limit; i++) {
+            if (this.usedIndices.has(i)) {
+                continue;
+            }
+
+            const x = this.parserOutput.ordered[i];
+            const [ok, y] = f(x.value);
+            if (ok) {
+                this.usedIndices.add(i);
+                ys.push(y!);
+            }
+        }
+
+        return ys;
+    }
+}
---
+++ test/args.test.ts
@@ -0,0 +1,140 @@
+import Parser from '../src/parser';
+import Lexer from '../src/lexer';
+import { longStrategy } from '../src/unordered';
+import Args from '../src/args';
+
+describe('args', () => {
+    it('can retrieve single and many args', () => {
+        const s = 'hello "world" baz "quux"';
+        const ts = new Lexer(s).setQuotes([['"', '"']]).lex();
+        const po = new Parser(ts).setUnorderedStrategy(longStrategy()).parse();
+        const args = new Args(po);
+
+        expect(args.single()).toEqual('hello');
+        expect(args.single()).toEqual('world');
+        expect(args.many()).toEqual([{ value: 'baz', trailing: ' ' }, { value: 'quux', quoted: '"quux"', trailing: '' }]);
+        expect(args.single()).toEqual(null);
+    });
+
+    it('can retrieve a limited amount of many args', () => {
+        const s = 'hello "world" baz "quux"';
+        const ts = new Lexer(s).setQuotes([['"', '"']]).lex();
+        const po = new Parser(ts).setUnorderedStrategy(longStrategy()).parse();
+        const args = new Args(po);
+
+        expect(args.many(2)).toEqual([{ value: 'hello', trailing: ' ' }, { value: 'world', quoted: '"world"', trailing: ' ' }]);
+        expect(args.single()).toEqual('baz');
+        expect(args.single()).toEqual('quux');
+        expect(args.single()).toEqual(null);
+    });
+
+    it('can retrieve flags and options', () => {
+        const s = '--foo --bar=123';
+        const ts = new Lexer(s).setQuotes([['"', '"']]).lex();
+        const po = new Parser(ts).setUnorderedStrategy(longStrategy()).parse();
+        const args = new Args(po);
+
+        expect(args.flag('foo')).toEqual(true);
+        expect(args.flag('hello')).toEqual(false);
+        expect(args.option('bar')).toEqual('123');
+        expect(args.option('world')).toEqual(null);
+    });
+
+    it('can retrieve do both things above', () => {
+        const s = 'hello "world" --foo --bar=123 baz "quux"';
+        const ts = new Lexer(s).setQuotes([['"', '"']]).lex();
+        const po = new Parser(ts).setUnorderedStrategy(longStrategy()).parse();
+        const args = new Args(po);
+
+        expect(args.single()).toEqual('hello');
+        expect(args.single()).toEqual('world');
+        expect(args.many()).toEqual([{ value: 'baz', trailing: ' ' }, { value: 'quux', quoted: '"quux"', trailing: '' }]);
+        expect(args.single()).toEqual(null);
+        expect(args.flag('foo')).toEqual(true);
+        expect(args.flag('hello')).toEqual(false);
+        expect(args.option('bar')).toEqual('123');
+        expect(args.option('world')).toEqual(null);
+    });
+
+    it('has the correct state and counts', () => {
+        const s = 'hello "world" baz "quux"';
+        const ts = new Lexer(s).setQuotes([['"', '"']]).lex();
+        const po = new Parser(ts).setUnorderedStrategy(longStrategy()).parse();
+        const args = new Args(po);
+
+        expect(args.length).toEqual(4);
+        expect(args.remaining).toEqual(4);
+        expect(args.finished).toEqual(false);
+        expect(args.usedIndices).toEqual(new Set());
+
+        args.single();
+        args.single();
+
+        expect(args.length).toEqual(4);
+        expect(args.remaining).toEqual(2);
+        expect(args.finished).toEqual(false);
+        expect(args.usedIndices).toEqual(new Set([0, 1]));
+
+        args.many();
+
+        expect(args.length).toEqual(4);
+        expect(args.remaining).toEqual(0);
+        expect(args.finished).toEqual(true);
+        expect(args.usedIndices).toEqual(new Set([0, 1, 2, 3]));
+    });
+
+    it('can find a token', () => {
+        const s = 'hello "world" baz "quux"';
+        const ts = new Lexer(s).setQuotes([['"', '"']]).lex();
+        const po = new Parser(ts).setUnorderedStrategy(longStrategy()).parse();
+        const args = new Args(po);
+
+        const y = args.findMap(x => x === 'hello' ? [true, 10] : [false, null]);
+        expect(y).toEqual(10);
+    });
+
+    it('cannot find a token', () => {
+        const s = 'hello "world" baz "quux"';
+        const ts = new Lexer(s).setQuotes([['"', '"']]).lex();
+        const po = new Parser(ts).setUnorderedStrategy(longStrategy()).parse();
+        const args = new Args(po);
+
+        const y = args.findMap(x => x === 'goodbye' ? [true, 10] : [false, null]);
+        expect(y).toEqual(null);
+    });
+
+    it('can filter multiple tokens', () => {
+        const s = 'hello "world" hello "quux"';
+        const ts = new Lexer(s).setQuotes([['"', '"']]).lex();
+        const po = new Parser(ts).setUnorderedStrategy(longStrategy()).parse();
+        const args = new Args(po);
+
+        const y = args.filterMap(x => x === 'hello' ? [true, 10] : [false, null]);
+        expect(y).toEqual([10, 10]);
+    });
+
+    it('will skip over a used token', () => {
+        const s = 'hello "world" baz "quux"';
+        const ts = new Lexer(s).setQuotes([['"', '"']]).lex();
+        const po = new Parser(ts).setUnorderedStrategy(longStrategy()).parse();
+        const args = new Args(po);
+
+        args.findMap(x => x === 'hello' ? [true, 10] : [false, null]);
+        expect(args.usedIndices).toEqual(new Set([0]));
+        expect(args.single()).toEqual('world');
+        expect(args.usedIndices).toEqual(new Set([0, 1]));
+        expect(args.position).toEqual(2);
+    });
+
+    it('will skip over multiple used tokens', () => {
+        const s = 'hello a hello b';
+        const ts = new Lexer(s).setQuotes([['"', '"']]).lex();
+        const po = new Parser(ts).setUnorderedStrategy(longStrategy()).parse();
+        const args = new Args(po);
+
+        args.filterMap(x => x === 'hello' ? [true, 10] : [false, null]);
+        expect(args.usedIndices).toEqual(new Set([0, 2]));
+        expect(args.many()).toEqual([{ value: 'a', trailing: ' ' }, { value: 'b', trailing: '' }]);
+        expect(args.usedIndices).toEqual(new Set([0, 1, 2, 3]));
+    });
+});
