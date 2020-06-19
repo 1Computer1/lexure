89cae07b38c72634a7c853f57fe27bcf2b20282e
1computer1 <22125769+1Computer1@users.noreply.github.com>
2020-06-19 11:55:17 -0400

    add support for esm imports

---
+++ esm/index.mjs
@@ -0,0 +1,26 @@
+import lexure from '../dist/lib/index.js';
+
+export const {
+    // args.js
+    Args,
+    // lexer.js
+    Lexer,
+    // option.js
+    some,
+    none,
+    // parser.js
+    Parser,
+    // parserOutput.js
+    outputFromJSON,
+    outputToJSON,
+    mergeOutputs,
+    emptyOutput,
+    // tokens.js
+    extractCommand,
+    joinTokens,
+    // unordered.js
+    noStrategy,
+    longStrategy,
+    longShortStrategy,
+    caseInsensitiveStrategy
+} = lexure;
