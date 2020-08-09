f82be92b705726c2d17fd4ba7c9b3ccb01d8f2a8
1computer1 <22125769+1Computer1@users.noreply.github.com>
2020-08-08 23:13:33 -0400

    benchmarks: add lexer benchmark

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -1,7 +1,20 @@
-const { Parser, mergeOutputs } = require('..');
+const { Parser, mergeOutputs, Lexer } = require('..');
 const { suite } = require('./wrapper');
 
 console.log('> Running benchmarks');
+
+suite('lexing', add => {
+    const string = Array.from({ length: 1000 }, i => {
+        const quoted = i % 11 == 0;
+        const trailing = ' '.repeat(i % 4 + 1);
+        return (quoted ? '"hey"' : 'hey') + trailing;
+    }).join('');
+
+    add('lexer.lex()', () => {
+        const lexer = new Lexer(string).setQuotes(['"', '"']);
+        lexer.lex();
+    });
+});
 
 suite('collecting parser output via mutations vs merging', add => {    
     const tokens = Array.from(
