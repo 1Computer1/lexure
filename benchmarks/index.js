aa6bc84a796c2ac219cfece4bcc47fa961541a32
1computer1 <22125769+1Computer1@users.noreply.github.com>
2020-08-08 23:45:55 -0400

    benchmarks: make it pretty

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -1,48 +1,48 @@
 const { Parser, mergeOutputs, Lexer, longShortStrategy, longStrategy } = require('..');
-const { suite } = require('./wrapper');
+const { benchmarks } = require('./wrapper');
 
-console.log('> Running benchmarks');
-
-suite('lexing', add => {
-    const string = Array.from({ length: 1000 }, i => {
-        const quoted = i % 11 == 0;
-        const trailing = ' '.repeat(i % 4 + 1);
-        return (quoted ? '"hey"' : 'hey') + trailing;
-    }).join('');
-
-    add('lexer.lex()', () => {
-        const lexer = new Lexer(string).setQuotes(['"', '"']);
-        lexer.lex();
-    });
-});
-
-suite('parsing', add => {
-    const tokens = Array.from({ length: 1000 }, i => {
-        const flag = i % 11 == 0;
-        const option = !flag && i % 13 == 0;
-        const s = flag ? '--hey' : option ? '--hey=there' : 'hey';
-        return { value: s, raw: s, trailing: ' ' };
+benchmarks(suite => {
+    suite('lexing with quotes', add => {
+        const string = Array.from({ length: 1000 }, i => {
+            const quoted = i % 11 == 0;
+            const trailing = ' '.repeat(i % 4 + 1);
+            return (quoted ? '"hey"' : 'hey') + trailing;
+        }).join('');
+    
+        add('lexer.lex()', () => {
+            const lexer = new Lexer(string).setQuotes(['"', '"']);
+            lexer.lex();
+        });
     });
 
-    add('parser.parse()', () => {
-        const parser = new Parser(tokens).setUnorderedStrategy(longStrategy());
-        parser.parse();
-    });
-});
+    suite('parsing with flags', add => {
+        const tokens = Array.from({ length: 1000 }, i => {
+            const flag = i % 11 == 0;
+            const option = !flag && i % 13 == 0;
+            const s = flag ? '--hey' : option ? '--hey=there' : 'hey';
+            return { value: s, raw: s, trailing: ' ' };
+        });
 
-suite('collecting parser output via mutations vs merging', add => {    
-    const tokens = Array.from(
-        { length: 1000 },
-        () => ({ value: 'hey', raw: 'hey', trailing: ' ' })
-    );
-
-    add('parser.parse()', () => {
-        const parser = new Parser(tokens);
-        parser.parse();
+        add('parser.parse()', () => {
+            const parser = new Parser(tokens).setUnorderedStrategy(longStrategy());
+            parser.parse();
+        });
     });
 
-    add('mergeOutputs(...parser)', () => {
-        const parser = new Parser(tokens);
-        mergeOutputs(...parser);
+    suite('collecting parser output via mutations vs merging', add => {    
+        const tokens = Array.from(
+            { length: 1000 },
+            () => ({ value: 'hey', raw: 'hey', trailing: ' ' })
+        );
+
+        add('parser.parse()', () => {
+            const parser = new Parser(tokens);
+            parser.parse();
+        });
+
+        add('mergeOutputs(...parser)', () => {
+            const parser = new Parser(tokens);
+            mergeOutputs(...parser);
+        });
     });
 });
--- benchmarks/wrapper.js
+++ benchmarks/wrapper.js
@@ -1,17 +1,40 @@
 const Benchmark = require('benchmark');
 
-function suite(name, fn) {
+function green(s) {
+    return `\x1B[90;42;1m${s}\x1B[0m`;
+}
+
+function bold(s) {
+    return `\x1B[1m${s}\x1B[0m`;
+}
+
+function benchmarks(fn) {
+    const suites = [];
+    fn((name, fn) => {
+        suites.push([name, makeSuite(fn)]);
+    });
+
+    for (const [name, suite] of suites) {
+        console.log(`${bold('>>>>')} ${name}`);
+        suite.run();
+        console.log('');
+    }
+
+    console.log('Benchmarks completed.');
+}
+
+function makeSuite(fn) {
     const suite = new Benchmark.Suite()
         .on('cycle', (event) => {
-            console.log(String(event.target));
+            console.log(`  ${String(event.target)}`);
         })
         .on('complete', function () {
-            console.log(`> Fastest is ${this.filter('fastest').map('name')}`);
+            const name = this.filter('fastest').map('name');
+            console.log(`${green(' OK ')} Fastest is ${bold(name)}`);
         });
 
     fn(suite.add.bind(suite));
-    console.log(`> Running benchmark - ${name}:`);
-    suite.run();
+    return suite;
 }
 
-module.exports = { suite };
+module.exports = { benchmarks };
